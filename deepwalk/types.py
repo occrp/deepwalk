550833b061b31c15c9d1a1db17faebd4f1d80791
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 12:44:09 +0000

    Adding tarfile member sanitization to extractall()

--- deepwalk/types.py
+++ deepwalk/types.py
@@ -75,7 +75,26 @@ class TarPackage(Package):
 
     def unpack(self):
         with tarfile.open(name=self.item.real_path, mode='r:*') as tf:
-            tf.extractall(self.temp_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, self.temp_path)
 
     def bid_file(self):
         if tarfile.is_tarfile(self.item.real_path):
